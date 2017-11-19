71fa14a0edc28dd834066f9f494c31394eebeed2
fbk4sure <iamddot@gmail.com>
2017-11-19 09:07:41 -0500

    first git commit

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'n3cSfvvh4QTXsWeqx1cv1GVlp'
+CONSUMER_SECRET = 'LoYL2NUA4iCaGp1QvthceqbmVXkQS45XWNlnQpMqj229FZB27f'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
